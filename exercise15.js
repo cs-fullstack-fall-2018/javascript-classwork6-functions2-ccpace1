877f8f94c7444598a9c038250c6adf825c375e90
Christopher Pace <Christopher_pace@ymail.com>
2018-07-31 13:18:13 -0500

    Ex16 added

--- exercise15.js
+++ exercise15.js
@@ -1,5 +1,18 @@
 function main() {
     var favNum = parseInt(prompt("What is your favorite number?"));
     alert("You're Favorite number is " + favNum);
+    return favNum
 }
-main();
+function aboveOrBelow100(number){
+if (number>100){
+    alert("Your number is over 100");
+}
+else if (number<100){
+    alert("Your number is under 100");
+}
+else{
+    alert("Your favorite number is 100")
+}
+}
+;
+aboveOrBelow100(main());
